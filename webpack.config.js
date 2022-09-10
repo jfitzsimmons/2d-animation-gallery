dba28f1926dbbff759bd95c55b084e26726a51d8
jfitzsimmons <jfitzcode@gmail.com>
2022-09-10 11:32:51 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/index.html',
+      filename: './index.html',
+      favicon: './assets/favicon.ico',
+    }),
+    new ESLintPlugin({
+      extensions: ['.tsx', '.ts', '.js'],
+      exclude: 'node_modules'
+   }),
+   new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  performance: {
+    maxEntrypointSize: 912000,
+    maxAssetSize: 912000,
+  },
+  devtool: 'source-map',
+  devServer: {
+    allowedHosts: 'auto',
+    client: {
+      overlay: true,
+    },
+    open: true,
+    port: 8000,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      publicPath: '/',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        }
+      },
+      {
+        test: /\.(sass|scss)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+}
