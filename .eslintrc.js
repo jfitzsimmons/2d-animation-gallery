3f40c53548b5c9e133b9a339b354a030ec9ecd40
jfitzsimmons <jfitzcode@gmail.com>
2022-09-11 09:59:00 -0500

    linting, prettier, ts classes abstraction

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    //sourceType: 'module', // Allows for the use of imports
   },
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
+/**
+ * testjpf
+ *
+ * separate circle and AnimationStage into two files
+ * AnimationStage will probably be index
+ * circle will be inanimations directory
+ * all animations share AnimationStage?!?!?
+ */
+
 import * as PIXI from 'pixi.js'
 import '../assets/style.scss'
 
-const rndmArrI = (a:any[]) => a[Math.floor(Math.random() * a.length)]
-const rndmRng = (h:number, l:number) => Math.random() * (h - l) + l
-const colors = [209, 291, 263]
-const strokeColors = ['506EE5', '68B2F8', '7037CD']
-const timeouts:any = []
+const rndmRng = (h: number, l: number) => Math.random() * (h - l) + l
+const colors: number[] = [209, 291, 263]
+const strokeColors: string[] = ['506EE5', '68B2F8', '7037CD']
+const timeouts: ReturnType<typeof setTimeout>[] = []
 
-function hslToHex(h: number, s:number, l:number) {
+function hslToHex(h: number, s: number, l: number) {
   l /= 100
   const a = (s * Math.min(l, 1 - l)) / 100
-  const f = (n:number) => {
+  const f = (n: number) => {
     const k = (n + h / 30) % 12
     const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1)
     return Math.round(255 * color)
@@ -20,9 +28,9 @@ function hslToHex(h: number, s:number, l:number) {
   return `0x${f(0)}${f(8)}${f(4)}`
 }
 
-function debounce<Params extends any[]>(
-  func: (...args: Params) => any,
-  timeout: number,
+function debounce<Params extends unknown[]>(
+  func: (...args: Params) => unknown,
+  timeout: number
 ): (...args: Params) => void {
   let timer: NodeJS.Timeout
   return (...args: Params) => {
@@ -34,193 +42,235 @@ function debounce<Params extends any[]>(
 }
 
 interface Bounds {
-  top: number,
-  bottom: number,
-  left:number,
-  right: number,
+  top: number
+  bottom: number
+  left: number
+  right: number
 }
 
+class Circle {
+  bounds: Bounds
+  cx: number
+  cy: number
+  start: number
+  speedX: number
+  speedY: number
+  radius: number
+  curr: number
+  innerCrcmf: number
+  grooves: number
+  color: number
+  light: number
+  strokeColor: string
+  graphics: PIXI.Graphics
 
-const Circle = function (this: any, bounds: Bounds) {
-  this.bounds = bounds
-  this.cx = Math.round(rndmRng(this.bounds.right - 15, this.bounds.left + 15))
-  this.cy = Math.round(rndmRng(this.bounds.bottom - 15, this.bounds.top + 15))
-  this.start = Math.random() * Math.PI * 2
-  this.speedX = Math.cos(this.start) / rndmRng(5, 1)
-  this.speedY = Math.sin(this.start) / rndmRng(5, 1)
-  this.radius = 0
-  this.curr = 0
-  this.innerCrcmf = rndmRng(130, 25)
-  this.grooves = rndmRng(35, 10)
-  this.color = rndmArrI(colors)
-  this.light = rndmRng(60, 10)
-  this.strokeColor = rndmArrI(strokeColors)
-  this.graphics = new PIXI.Graphics()
-  this.graphics.blendMode = PIXI.BLEND_MODES.XOR
-}
-
-Circle.prototype.update = function () {
-  if (this.radius < this.innerCrcmf && typeof this.radius !== 'undefined') {
-    this.radius += Math.round(this.innerCrcmf / this.grooves)
-    this.graphics.beginFill(
-      hslToHex(
-        Math.round((this.color += 0.3)),
-        Math.round(100 - rndmRng(50, 0)),
-        Math.round((this.light += 0.1))
-      )
-    )
-
-    this.graphics.drawCircle(this.cx, this.cy, this.radius)
-    return this.graphics
+  constructor(bounds: Bounds) {
+    this.bounds = bounds
+    this.cx = Math.round(rndmRng(this.bounds.right - 15, this.bounds.left + 15))
+    this.cy = Math.round(rndmRng(this.bounds.bottom - 15, this.bounds.top + 15))
+    this.start = Math.random() * Math.PI * 2
+    this.speedX = Math.cos(this.start) / rndmRng(5, 1)
+    this.speedY = Math.sin(this.start) / rndmRng(5, 1)
+    this.radius = 0
+    this.curr = 0
+    this.innerCrcmf = rndmRng(130, 25)
+    this.grooves = rndmRng(35, 10)
+    this.color = colors[Math.floor(Math.random() * colors.length)]
+    this.light = rndmRng(60, 10)
+    this.strokeColor =
+      strokeColors[Math.floor(Math.random() * strokeColors.length)]
+    this.graphics = new PIXI.Graphics()
+    this.graphics.blendMode = PIXI.BLEND_MODES.XOR
   }
-
-  if (this.curr < 101) {
-    this.graphics
-      .arc(
-        this.cx,
-        this.cy,
-        Math.round(this.innerCrcmf + rndmRng(45, 10)),
-        this.start,
-        (Math.PI * 2 * this.curr) / 100 + this.start,
-        false
+  update() {
+    if (this.radius < this.innerCrcmf && typeof this.radius !== 'undefined') {
+      this.radius += Math.round(this.innerCrcmf / this.grooves)
+      this.graphics.beginFill(
+        parseInt(
+          hslToHex(
+            Math.round((this.color += 0.3)),
+            Math.round(100 - rndmRng(50, 0)),
+            Math.round((this.light += 0.1))
+          )
+        )
       )
-      .lineStyle(Math.round(rndmRng(25, 5)), `0x${this.strokeColor}`)
-    this.curr += rndmRng(8.8, 3.4)
-    if (this.curr == 100) this.graphics.cacheAsBitmap = true
-  } else {
-    if (
-      this.graphics.x + this.cx + this.innerCrcmf > this.bounds.right ||
-      this.graphics.x + this.cx < this.bounds.left
-    ) {
-      this.speedX *= -1
-    } else if (
-      this.graphics.y + this.cy < this.bounds.top ||
-      this.graphics.y + this.cy + this.innerCrcmf > this.bounds.bottom
-    ) {
-      this.speedY *= -1
+
+      this.graphics.drawCircle(this.cx, this.cy, this.radius)
+      return this.graphics
     }
 
-    this.graphics.x -= this.speedX
-    this.graphics.y -= this.speedY
+    if (this.curr < 101) {
+      this.graphics
+        .arc(
+          this.cx,
+          this.cy,
+          Math.round(this.innerCrcmf + rndmRng(45, 10)),
+          this.start,
+          (Math.PI * 2 * this.curr) / 100 + this.start,
+          false
+        )
+        .lineStyle(
+          Math.round(rndmRng(25, 5)),
+          parseInt(`0x${this.strokeColor}`)
+        )
+      this.curr += rndmRng(8.8, 3.4)
+      if (this.curr == 100) this.graphics.cacheAsBitmap = true
+    } else {
+      if (
+        this.graphics.x + this.cx + this.innerCrcmf > this.bounds.right ||
+        this.graphics.x + this.cx < this.bounds.left
+      ) {
+        this.speedX *= -1
+      } else if (
+        this.graphics.y + this.cy < this.bounds.top ||
+        this.graphics.y + this.cy + this.innerCrcmf > this.bounds.bottom
+      ) {
+        this.speedY *= -1
+      }
+
+      this.graphics.x -= this.speedX
+      this.graphics.y -= this.speedY
+    }
   }
 }
 
-const CircleStage = function (this: any, domElementSelector: string) {
-  this.circles = []
+class Circles {
+  circles: Circle[]
 
-  this.domElement = document.getElementById(domElementSelector)
-
-  this.bounds = {
-    left: 0,
-    top: 0,
-    right: 0,
-    bottom: 0,
+  constructor() {
+    this.circles = []
   }
 
-  this.renderer = null
+  init(bounds: Bounds) {
+    const circleAmount = Math.round((bounds.right * bounds.bottom) / 47000)
+    for (let i = circleAmount; i--; ) {
+      timeouts.push(
+        setTimeout(() => {
+          const circle = new Circle(bounds)
+          this.circles.push(circle)
+        }, i * rndmRng(2000, 900))
+      )
+    }
+  }
 
-  this.stage = null
+  update() {
+    if (this.circles.length > 0) {
+      for (let i = this.circles.length; i--; ) {
+        const child = this.circles[i].update()
+        if (child) AnimationStage.stage.addChild(child)
+      }
+    }
+  }
+
+  reset() {
+    for (const to in timeouts) {
+      window.clearTimeout(to)
+    }
+    AnimationStage.stage.removeChildren()
+    this.circles.length = 0
+    this.init(AnimationStage.bounds)
+  }
 }
 
+class AnimationStage {
+  static bounds: Bounds
+  static getBounds() {
+    return this.bounds
+  }
+  static stage: PIXI.Container
+  static getStage() {
+    return this.stage
+  }
+  /**
+   * testjpf
+   *
+   * track list of animations (startfunctions per animation?!?!?)
+   * track current animation (with default)
+   */
+  domElement: HTMLElement
+  renderer: PIXI.AbstractRenderer
+  currentAnimation: Circles
 
-CircleStage.prototype.addCircles = function () {
-  const circleAmount = Math.round(
-    (this.bounds.right * this.bounds.bottom) / 47000
-  )
-  for (let i = circleAmount; i--; ) {
-    timeouts.push(
-      setTimeout(() => {
-        const circle = new (Circle as any)(this.bounds)
-        this.circles.push(circle)
-      }, i * rndmRng(2000, 900))
-    )
+  constructor(domElementSelector: string) {
+    this.domElement = document.getElementById(domElementSelector)
+
+    AnimationStage.bounds = {
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+    }
+
+    this.currentAnimation = new Circles()
+  }
+
+  ready() {
+    if (typeof PIXI === 'undefined') {
+      this.domElement.classList.add('error')
+      throw 'PIXI is required to run'
+    }
+
+    const stage = document.getElementById('cvs0') as HTMLCanvasElement
+
+    AnimationStage.bounds.right = stage.offsetWidth
+    AnimationStage.bounds.bottom = stage.offsetHeight
+
+    const options = {
+      backgroundAlpha: 0,
+      view: stage,
+      clearBeforeRender: true,
+    }
+
+    Object.assign(options, {
+      width: AnimationStage.bounds.right,
+      height: AnimationStage.bounds.bottom,
+    })
+
+    try {
+      this.renderer = PIXI.autoDetectRenderer(options)
+    } catch (err) {
+      alert(err.message)
+      return
+    }
+
+    AnimationStage.stage = new PIXI.Container()
+    AnimationStage.stage.interactiveChildren = false
+
+    window.addEventListener('resize', debounce(this.resize.bind(this), 400))
+
+    this.startUpdate()
+
+    this.currentAnimation.init(AnimationStage.bounds)
+  }
+  startUpdate() {
+    requestAnimationFrame(() => this.update())
+  }
+  update() {
+    this.currentAnimation.update()
+
+    this.renderer.render(AnimationStage.stage)
+
+    this.startUpdate()
+  }
+  resize() {
+    const prevWidth = AnimationStage.bounds.right
+    const prevHeight = AnimationStage.bounds.bottom
+    const width = this.domElement.offsetWidth
+    const height = this.domElement.offsetHeight
+    AnimationStage.bounds.right = width
+    AnimationStage.bounds.bottom = height
+    if (
+      Math.abs(prevWidth - width) > 50 ||
+      Math.abs(prevHeight - height) > 50
+    ) {
+      this.renderer.resize(width, height)
+      this.currentAnimation.reset()
+    }
   }
 }
 
-CircleStage.prototype.ready = function () {
-  if (typeof PIXI === 'undefined') {
-    this.domElement.addClass('error')
-    throw 'PIXI is required to run'
-  }
-
-  const stage = document.getElementById('cvs0') as HTMLCanvasElement
-
-  this.bounds.right = stage.offsetWidth
-  this.bounds.bottom = stage.offsetHeight
-
-  const options = {
-    backgroundAlpha: 0,
-    view: stage,
-    clearBeforeRender: true,
-  }
-
-  Object.assign(options, {
-    width: this.bounds.right,
-    height: this.bounds.bottom,
-  })
-
-  try {
-    this.renderer = PIXI.autoDetectRenderer(options)
-  } catch (err) {
-    alert(err.message)
-    return
-  }
-
-  this.stage = new PIXI.Container()
-  this.stage.interactiveChildren = false
-
-  window.addEventListener('resize', debounce(this.resize.bind(this), 400))
-
-  this.startUpdate()
-  this.addCircles()
-}
-
-document.addEventListener('DOMContentLoaded', (e) => {
-  const app = new (CircleStage as any)('cvs0-container')
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new AnimationStage('cvs0-container')
 
   app.ready()
 })
-
-CircleStage.prototype.startUpdate = function () {
-  // testjpf this below breaks app!!!! 
-  // eslint-disable-next-line @typescript-eslint/no-this-alias
-  const _this = this
-  requestAnimationFrame(function () {
-    _this.update()
-  })
-}
-
-CircleStage.prototype.resize = function () {
-  const prevWidth = this.bounds.right
-  const prevHeight = this.bounds.bottom
-  const width = this.domElement.offsetWidth
-  const height = this.domElement.offsetHeight
-  this.bounds.right = width
-  this.bounds.bottom = height
-  if (Math.abs(prevWidth - width) > 50 || Math.abs(prevHeight - height) > 50) {
-    this.renderer.resize(width, height)
-    this.reset()
-  }
-}
-
-CircleStage.prototype.reset = function () {
-  for (const to in timeouts) {
-    window.clearTimeout(to)
-  }
-  this.stage.removeChildren()
-  this.circles.length = 0
-  this.addCircles()
-}
-
-CircleStage.prototype.update = function () {
-  if (this.circles.length > 0) {
-    for (let i = this.circles.length; i--; ) {
-      const child = this.circles[i].update()
-      if (child) this.stage.addChild(child)
-    }
-  }
-
-  this.renderer.render(this.stage)
-
-  this.startUpdate()
-}
